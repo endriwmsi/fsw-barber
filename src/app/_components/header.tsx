f22543a7f85fbcf26471f6937719f17cf8aa8120
endriwmsi <endriwmsi@gmail.com>
2024-08-09 20:16:23 -0300

    feat: add sidebar

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image"
-import { Card, CardContent } from "./ui/card"
-import { Button } from "./ui/button"
-import { MenuIcon } from "lucide-react"
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { quickSearchOptions } from "../_constants/quickSearchOptions";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -9,12 +20,76 @@ const Header = () => {
       <CardContent className="flex items-center justify-between p-5">
         <Image alt="FSW Barber" src="/logo.png" width={120} height={18} />
 
-        <Button size="icon" variant="ghost">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="ghost">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
+
+            <div className="flex items-center gap-3 border-b border-solid py-5">
+              <Avatar className="rounded-full border-2 border-primary">
+                <AvatarImage
+                  src="https://www.endriwmsi.com/assets/endriw-DWKK9I3f.jpg"
+                  className="object-cover"
+                />
+              </Avatar>
+
+              <div className="flex flex-col">
+                <p className="font-bold">Endriw Schiavenato</p>
+                <p className="text-xs text-gray-400">endriwmsi@gmail.com</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              <SheetClose asChild>
+                <Button className="justify-start gap-2" variant="ghost" asChild>
+                  <Link href="/">
+                    <HomeIcon size={18} />
+                    Inicio
+                  </Link>
+                </Button>
+              </SheetClose>
+              <Button className="justify-start gap-2" variant="ghost">
+                <CalendarIcon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-2 border-b border-solid py-5">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  className="justify-start gap-2"
+                  variant="ghost"
+                  key={option.title}
+                >
+                  <Image
+                    alt={option.title}
+                    src={option.imageUrl}
+                    height={18}
+                    width={18}
+                  />
+                  {option.title}
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 py-5">
+              <Button className="justify-start gap-2" variant="ghost">
+                <LogOutIcon size={18} />
+                Sair
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
