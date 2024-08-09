f3f56b71947360d6b464555c62b9bdbcac074d5f
endriwmsi <endriwmsi@gmail.com>
2024-08-09 01:37:21 -0300

    feat: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
-import { SearchIcon } from "lucide-react"
-import Header from "./_components/header"
-import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
-import Image from "next/image"
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
+import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar } from "./_components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
-const Home = () => {
+const Home = async () => {
   return (
     <div>
       {/* header */}
       <Header />
 
+      {/* texto */}
       <div className="p-5">
         <h2 className="text-xl">
           Olá,
@@ -17,6 +22,7 @@ const Home = () => {
         </h2>
         <p>Segunda-feira, 08 de agosto.</p>
 
+        {/* busca */}
         <div className="mt-6 flex items-center gap-2 space-x-2">
           <Input placeholder="Faça sua busca..." />
 
@@ -25,6 +31,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -33,9 +40,36 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* agendamentos */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage src="https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">14:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
