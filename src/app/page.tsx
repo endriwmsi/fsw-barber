e1798459960ffa86ae829d999057fd7d85b28705
endriwmsi <endriwmsi@gmail.com>
2024-08-09 10:41:07 -0300

    Revert "feat: add booking-item component"
    This reverts commit d8f7740e2985120ddb2f6cff3ae7fd3b3c4d2d24.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar } from "./_components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
-import BookingItem from "./_components/booking-item";
-import { quickSearchOptions } from "./_constants/quickSearchOptions";
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({});
@@ -41,17 +42,45 @@ const Home = async () => {
 
         {/* busca rapida */}
         <div className="mt-6 flex items-center gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          {quickSearchOptions.map((option) => (
-            <Button className="gap-2" variant="secondary" key={option.title}>
-              <Image
-                alt={option.title}
-                src={option.imageUrl}
-                width={16}
-                height={16}
-              />
-              <span>{option.title}</span>
-            </Button>
-          ))}
+          <Button className="gap-2" variant="secondary">
+            <Image alt="Cabelo" src="/cabelo.svg" width={16} height={16} />
+            <span>Cabelo</span>
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image alt="Barba" src="/barba.svg" width={16} height={16} />
+            <span>Barba</span>
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image
+              alt="Sobrancelha"
+              src="/sobrancelha.svg"
+              width={16}
+              height={16}
+            />
+            <span>Sobrancelha</span>
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image
+              alt="Acabamento"
+              src="/acabamento.svg"
+              width={16}
+              height={16}
+            />
+            <span>Acabamento</span>
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image alt="Massagem" src="/massagem.svg" width={16} height={16} />
+            <span>Massagem</span>
+          </Button>
+          <Button className="gap-2" variant="secondary">
+            <Image
+              alt="Hidratação"
+              src="/hidratacao.svg"
+              width={16}
+              height={16}
+            />
+            <span>Hidratação</span>
+          </Button>
         </div>
 
         {/* banner */}
@@ -65,7 +94,31 @@ const Home = async () => {
         </div>
 
         {/* agendamentos */}
-        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage src="https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">14:00</p>
+            </div>
+          </CardContent>
+        </Card>
 
         {/* recomendados */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
